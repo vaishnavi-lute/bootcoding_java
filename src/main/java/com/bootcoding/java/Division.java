15e77f52de87141cfc0ae33a3e2c53157be79dd2
vaishnavi_lute <vaishanvilute.b@gmail.com>
2023-07-25 11:56:02 +0530

    Evenodd

---
+++ src/main/java/com/bootcoding/java/Division.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java;
+
+public class Division {
+    public static void main(String[] args) {
+        int a = 87;
+        if (a % 3 == 0) {
+            System.out.println("Divisible by 3");
+        } else {
+            System.out.println("Not divisible by 3");
+        }
+    }
+}
---
+++ src/main/java/com/bootcoding/java/Operator.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
