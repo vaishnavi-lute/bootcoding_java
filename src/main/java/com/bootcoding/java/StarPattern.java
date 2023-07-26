663e833af3473b866fe480e52003ec01c0a9a99b
vaishnavi_lute <vaishanvilute.b@gmail.com>
2023-07-26 16:27:48 +0530

    java programs of star pattern

---
+++ src/main/java/com/bootcoding/java/StarPattern.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        int n=5;
+        int i,j;
+        for(i=1; i<=n;i++){
+            for( j=1; j<=n; j++){
+                System.out.print("  *  ");
+            }
+            System.out.println();
+        }
+    }
+}
