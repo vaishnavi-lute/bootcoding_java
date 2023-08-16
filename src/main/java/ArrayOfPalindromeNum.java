f8c8291de29ae46d0f8d60c1fa09b8b3cd4dd869
vaishnavi_lute <vaishanvilute.b@gmail.com>
2023-08-16 14:51:29 +0530

    java program of array of palindrome.

--- src/main/java/ArrayOfPalindromeNum.java
+++ src/main/java/ArrayOfPalindromeNum.java
@@ -1,21 +1,19 @@
 public class ArrayOfPalindromeNum {
-    public static void main(String[] args) {
-        int a[]={121,133,141,155,161,252};
-        int i=0;
-        int temp=0;
-        int reverse=0;
-        int rem=0;
-        while(i<a.length) {
-            rem = temp % 10;
-            reverse = (reverse * 10) + rem;
-            temp=temp/10;
+        public static boolean isPalindrome(int arr[]) {
+            for (int i = 0; i < arr.length / 2; i++) {
+                if (arr[i] != arr[arr.length - 1 - i]) {
+                    return false;
+                }
+            }
+            return true;
         }
-        if(reverse == a[i]){
-            System.out.println(a[i] +"  is palindrome no");
-        }
-        else{
-            System.out.println( a[i] + "  is not palindrome no");
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,3,2,1};
+            boolean ans = isPalindrome(arr);
+            if (ans) {
+                System.out.println("palindrome array");
+            } else {
+                System.out.println("not an palindrome array");
+            }
         }
     }
-    }
-
