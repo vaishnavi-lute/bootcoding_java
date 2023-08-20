6a076ea84ab39bf56de293b903574d8775f8dfb1
vaishnavi_lute <vaishanvilute.b@gmail.com>
2023-08-20 13:22:43 +0530

    java program using method.

---
+++ src/main/java/SwapByMethod.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SwapByMethod {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        SwapTwoNumbers(a,b);
+    }
+    public static void SwapTwoNumbers(int a, int b){
+        System.out.println("before swapping numbers: "+a +"  " + b);
+        int temp=0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: "+a + "    " + b);
+    }
+}
