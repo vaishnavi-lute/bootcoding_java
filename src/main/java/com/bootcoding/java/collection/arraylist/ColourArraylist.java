b386f693ef7e7142f2a9c768580eacb9c92e1500
vaishnavi_lute <vaishanvilute.b@gmail.com>
2023-11-08 15:07:20 +0530

    java programs of Arraylist.

---
+++ src/main/java/com/bootcoding/java/collection/arraylist/ColourArraylist.java
@@ -0,0 +1,20 @@
+package com.bootcoding.java.collection.arraylist;
+
+import java.util.ArrayList;
+
+public class ColourArraylist {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("red");
+        System.out.println(arrayList);
+
+        arrayList.add("blue");
+        System.out.println(arrayList);
+
+        arrayList.add("black");
+        System.out.println(arrayList);
+
+        arrayList.add("white");
+        System.out.println(arrayList);
+    }
+}
